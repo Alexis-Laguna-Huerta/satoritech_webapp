f5dddcb7139c4a1484ee56bed03153bbf9825dd7
AlexisLaguna <alexisoalh.13952@gmail.com>
2026-04-23 18:25:05 -0500

    Speech recognition

--- src/app/components/welcome-form/welcome-form.ts
+++ src/app/components/welcome-form/welcome-form.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 
+declare const window: any;
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
 @Component({
   selector: 'app-welcome-form',
   imports: [ReactiveFormsModule],
@@ -15,6 +18,8 @@ export class WelcomeForm {
   ]);
 
   isLoading = false;
+  isRecording = false;
+  recognition: any;
 
   get nameLength(): number {
     return this.nameControl.value?.length || 0;
@@ -31,6 +36,48 @@ export class WelcomeForm {
     }
 
     this.nameControl.setValue(value, { emitEvent: false });
+  }
+
+  toggleDictation() {
+    if (this.isRecording) {
+      this.recognition?.stop();
+      this.isRecording = false;
+      return;
+    }
+
+    this.recognition = new SpeechRecognition();
+    this.recognition.lang = 'es-MX';
+    this.recognition.interimResults = false;
+    this.recognition.maxAlternatives = 1;
+
+    this.recognition.onstart = () => {
+      this.isRecording = true;
+    };
+
+    this.recognition.onresult = (event: any) => {
+      const transcript = event.results[0][0].transcript;
+
+      // Aplicar misma lógica de filtrado
+      let value = transcript.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g, '');
+      if (value.length > 15) {
+        value = value.substring(0, 15);
+      }
+
+      this.nameControl.setValue(value);
+      this.nameControl.markAsDirty();
+      this.nameControl.markAsTouched();
+    };
+
+    this.recognition.onerror = (event: any) => {
+      console.error("Error en dictado de voz: ", event.error);
+      this.isRecording = false;
+    };
+
+    this.recognition.onend = () => {
+      this.isRecording = false;
+    };
+
+    this.recognition.start();
   }
 
   onSubmit() {
